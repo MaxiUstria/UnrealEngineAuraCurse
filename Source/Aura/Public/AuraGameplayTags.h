bbadf87063310ff042596503c45ea16301c27ece
Your Name <maxiustria@gmail.com>
2026-01-05 01:05:01 -0300

    Gameplay tags singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * Struct to store all the gameplay tags for the game
+ * 
+ * This is a singleton class that is used to store all the gameplay tags for the game
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+
+	static void InitializeNativeGameplayTags();
+
+protected:
+
+private:
+    static FAuraGameplayTags GameplayTags;
+};
